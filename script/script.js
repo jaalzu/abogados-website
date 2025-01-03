f6cb92d77e9c9481b03e56cfb1f78a73994fb113
Javier <javieralzuu@gmail.com>
2025-01-03 19:53:26 -0300

    nav done

--- script/script.js
+++
@@ -1,9 +0,0 @@
-const btnMenu = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-
-btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('show-menu')
-})
-
-
-
