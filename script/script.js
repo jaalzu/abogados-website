565d8598bfc3f1e8e351d7e41b5623c35ebcf899
Javier <javieralzuu@gmail.com>
2025-01-20 18:21:33 -0300

    scroll reveal index done

---
+++ script/script.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.intro__box',{delay:600,origin:'right',distance: '15px'});
+
+ScrollReveal().reveal('.services__flex',{delay:400,origin:'left',distance: '10px'});
+
+ScrollReveal().reveal('.services__grid',{delay:600,origin:'right',distance: '10px'});
+
+ScrollReveal().reveal('.section__title',{delay:600,origin:'right',distance: '10px'});
+
+ScrollReveal().reveal('.about__flex',{delay:500,origin:'left',distance: '10px'});
+
+ScrollReveal().reveal('.about__grid',{delay:500,origin:'right',distance: '10px'});
+
+
+ScrollReveal().reveal('.value__flex',{delay:500,origin:'left',distance: '10px'});
+ScrollReveal().reveal('.value__grid',{delay:500,origin:'right',distance: '10px'});
+
+
+ScrollReveal().reveal('.contact__text',{delay:500,origin:'bottom',distance: '10px'});
+ScrollReveal().reveal('.contact__grid',{delay:500,origin:'left',distance: '10px'});
