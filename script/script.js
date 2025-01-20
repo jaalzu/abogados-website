fd8d2f15597d1db29a8be2edcfbc49b834f1ea7e
Javier <javieralzuu@gmail.com>
2025-01-20 19:11:23 -0300

    scroll reveal done for nosotros page

--- script/script.js
+++ script/script.js
@@ -1,19 +1,41 @@
+// scroll reveal for index start
+
+// scroll reveal intro section
 ScrollReveal().reveal('.intro__box',{delay:600,origin:'right',distance: '15px'});
 
+// scroll reveal services section
+ScrollReveal().reveal('.section__title',{delay:600,origin:'right',distance: '10px'});
 ScrollReveal().reveal('.services__flex',{delay:400,origin:'left',distance: '10px'});
-
 ScrollReveal().reveal('.services__grid',{delay:600,origin:'right',distance: '10px'});
 
-ScrollReveal().reveal('.section__title',{delay:600,origin:'right',distance: '10px'});
-
+// scroll reveal about us section
 ScrollReveal().reveal('.about__flex',{delay:500,origin:'left',distance: '10px'});
-
 ScrollReveal().reveal('.about__grid',{delay:500,origin:'right',distance: '10px'});
 
 
+// scroll reveal our value section
 ScrollReveal().reveal('.value__flex',{delay:500,origin:'left',distance: '10px'});
 ScrollReveal().reveal('.value__grid',{delay:500,origin:'right',distance: '10px'});
 
 
+// scroll reveal contact section
 ScrollReveal().reveal('.contact__text',{delay:500,origin:'bottom',distance: '10px'});
 ScrollReveal().reveal('.contact__grid',{delay:500,origin:'left',distance: '10px'});
+
+// scroll reveal for index end
+
+
+
+
+
+
+// scroll reveal for nosotros page start //
+
+ScrollReveal().reveal('.nosotros__description',{delay:500,origin:'left',distance: '10px',interval:200});
+ScrollReveal().reveal('.nosotros__img',{delay:500,origin:'right',distance: '10px',interval:200});
+
+
+ScrollReveal().reveal('.lawyer__description',{delay:500,origin:'right',distance: '15px',interval:200});
+ScrollReveal().reveal('.lawyer__img',{delay:500,origin:'left',distance: '15px',interval:200});
+
+// scroll reveal for nosotros page end //
